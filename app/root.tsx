7bdc95e87a7ac0aaab408de121dca6dfa6b72669
Ola Rybacka <ola.rybacka@platform24.se>
2021-11-24 00:10:09 +0100

    style

--- app/root.tsx
+++ app/root.tsx
@@ -1,18 +1,15 @@
-import { LiveReload, Outlet } from "remix";
-
+import { LiveReload, Outlet , Links} from 'remix';
 export default function App() {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <title>Remix: Hello world</title>
+        <Links />
       </head>
       <body>
-
         <Outlet />
-        {process.env.NODE_ENV === "development" ? (
-          <LiveReload />
-        ) : null}
+        {process.env.NODE_ENV === 'development' ? <LiveReload /> : null}
       </body>
     </html>
   );
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,2 +1,8 @@
+import type { LinksFunction } from "remix";
+import stylesUrl from "../styles/index.css";
+
+export let links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: stylesUrl }];
+};
 const IndexRoute = () => <div>Index route</div>;
 export default IndexRoute;
